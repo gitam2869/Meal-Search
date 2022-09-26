7a63fc1f7f84e599ea9e90bcef8aa4f079fb8556
gitam <gautamhelange025@gmail.com>
2022-09-26 11:58:58 +0530

    App setup

--- app/src/main/java/com/app/mealsearch/MainActivity.kt
+++ app/src/main/java/com/app/mealsearch/MainActivity.kt
@@ -2,10 +2,21 @@ package com.app.mealsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.app.mealsearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
+        setContentView(binding.root)
+
+        binding.tv.apply {
+            text = "Hello Clean Arch."
+        }
     }
 }
