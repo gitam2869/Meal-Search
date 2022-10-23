7d5e65afeb81e3b3acca83ee2c6cb6b97c04a6d9
gitam2869 <gautamhelange025@gmail.com>
2022-10-23 18:05:34 +0530

    Added back button in meal details page

---
+++ app/src/main/java/com/app/mealsearch/presentation/mealdetails/BaseFragment.kt
@@ -0,0 +1,8 @@
+package com.app.mealsearch.presentation.mealdetails
+
+import androidx.fragment.app.Fragment
+import com.app.mealsearch.databinding.ActivityMainBinding
+
+open class BaseFragment : Fragment() {
+    var activityMainBinding: ActivityMainBinding? = null
+}
